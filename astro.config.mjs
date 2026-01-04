7ee96560389494f5c902edbaef1fcb2a391c1ff3
jk123x <jay.kinkade@gmail.com>
2026-01-04 14:01:38 +1100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: 'hybrid',
+  site: 'https://solarmath.com.au',
+  integrations: [react(), tailwind(), sitemap()],
+  output: 'server',
   adapter: vercel()
 });
